740fab57168beacfa248a6d9eed567d59a1add97
Amal M <amalmhnofficial@gmail.com>
2021-08-10 21:07:51 +0530

    about completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Modal, Nav, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Header.css'
 
 function Header() {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div className="header-div">
             <Navbar variant="light">
@@ -16,17 +22,35 @@ function Header() {
                         </Link>
                     </Nav>
                     <Nav>
-                        <Link to="/tours" className="headerLink">
-                            <span className="header-span">About</span>
+                        <Link className="headerLink">
+                            <span onClick={handleShow} className="header-span">About</span>
                         </Link>
                     </Nav>
                     <Nav>
                         <Link to="/contact" className="headerLink">
-                        <span className="header-span">Contact</span>
+                            <span className="header-span">Contact</span>
                         </Link>
-                        </Nav>
+                    </Nav>
                 </Nav>
             </Navbar>
+            <div className="header-modal">
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title> <h2>About</h2> </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h4>Explore</h4>
+                        <p>
+                        When you travel with Explore, you’re also giving back. Explore is committed to sustainable travel and minimizing its impact on the environment and to reducing its dependence on non-renewable resources. In this endeavor, we have implemented policies to support this sustainable travel mission and are working carefully to enhance our products with this mission and our values combined. Explore maintains a policy of "Leave Only Footprints and Take Only Photos."
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button className="modal-btn" variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
         </div>
     )
 }
